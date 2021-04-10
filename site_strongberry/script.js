1d70b7aedbbd5185b79964c19561f54c087a3571
LucasTeixeira793 <lucas.teixeira@bandtec.com>
2021-04-10 01:22:32 -0300

    HTML do simulador financeiro bonito

---
+++ site_strongberry/script.js
@@ -0,0 +1,13 @@
+function adicionarEmail(){
+    var email = inputEmailNoticias.value;
+    if (email != "") {
+      alert(`Obrigado pela preferência`);
+    }else{
+      alert(`Preencha um email válido`);
+    }    
+}
+
+function calcularLucro(){
+    var metros = Number(inputMetros2.value);
+    var mudas = Number(inputMudasPorM2.value);
+}
