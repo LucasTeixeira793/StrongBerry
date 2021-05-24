1f2c8998ccd44a280e70ca9f5f322eedc1527f06
JvRuas <contato.jvruas@gmail.com>
2021-05-24 14:01:03 -0300

    Implementando novas Alterações na Modelagem do Banco de Dados e criando um novo usuario para insert de dados dos sensores

--- API-envia-dados/app.js
+++ API-envia-dados/app.js
@@ -1,22 +1,22 @@
-process.env.NODE_ENV = 'dev'; // ALTERE AQUI seu ambiente para 'dev' ou 'production'
+process.env.NODE_ENV = "dev"; // ALTERE AQUI seu ambiente para 'dev' ou 'production'
 
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var apiSensores = require('./routes/api-sensores');
+var indexRouter = require("./routes/index");
+var apiSensores = require("./routes/api-sensores");
 
 var app = express();
 
-app.use(logger('dev')); 
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/api', apiSensores);
+app.use("/", indexRouter);
+app.use("/api", apiSensores);
 
 module.exports = app;
--- API-envia-dados/config/config.js
+++ API-envia-dados/config/config.js
@@ -25,8 +25,8 @@ module.exports = {
     // Insira aqui seus dados do banco LOCAL - MySQL Workbench
     dev: {
         // altere APENAS username, password e database.
-        username: "user-strongberry",
-        password: "strongberrysenha",
+        username: "insert_sensor_data",
+        password: "insertdatasenha",
         database: "bdStrongBerry",
         host: "localhost",
         dialect: "mysql",
