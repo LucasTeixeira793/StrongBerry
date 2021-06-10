d3d318b85bd2c9a9711803d03b656fd21334a0a1
LucasTeixeira793 <lucas.teixeira@bandtec.com>
2021-06-10 17:51:15 -0300

    Azure Deploy Branch

---
+++ config/config.js
@@ -0,0 +1,47 @@
+module.exports = {
+    // Insira aqui seus dados do banco NA NUVEM AZURE
+    production: {
+        // altere APENAS username, password, database e host.
+        username: "servidorStrongberry",
+        password: "#strong#berry8",
+        database: "bdStrongberry",
+        host: "servidor-strongberry.database.windows.net",
+        dialect: "mssql",
+        xuse_env_variable: "DATABASE_URL",
+        dialectOptions: {
+            options: {
+                encrypt: true,
+            },
+        },
+        pool: {
+            max: 5,
+            min: 1,
+            acquire: 5000,
+            idle: 30000,
+            connectTimeout: 5000,
+        },
+    },
+
+    // Insira aqui seus dados do banco LOCAL - MySQL Workbench
+    dev: {
+        // altere APENAS username, password e database.
+        username: "user-strongberry",
+        password: "strongberrysenha",
+        database: "bdstrongberry",
+        host: "localhost",
+        dialect: "mysql",
+        xuse_env_variable: "DATABASE_URL",
+        dialectOptions: {
+            options: {
+                encrypt: true,
+            },
+        },
+        pool: {
+            max: 5,
+            min: 1,
+            acquire: 5000,
+            idle: 30000,
+            connectTimeout: 5000,
+        },
+    },
+};
